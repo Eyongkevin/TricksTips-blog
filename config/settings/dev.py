c41b2b3bd3e4051315c4e105cd40ae81bf2fd1e3
Eyong Kevin Enowanyo <tonyparkerkenz@gmail.com>
2022-10-22 10:15:33 +0100

    tech: configure base and dev setting files

---
+++ config/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+import os
+import environ
+
+env = environ.Env(DEBUG=(bool, True))
+environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
+
+SECRET_KEY = env.str("SECRET_KEY")
+DEBUG = env.bool("DEBUG")
+
+ALLOWED_HOSTS = []
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
