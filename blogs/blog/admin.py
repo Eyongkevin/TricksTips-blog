8478660b5f4b3a2bd71fa48929eb506b32a56aac
Eyong Kevin Enowanyo <tonyparkerkenz@gmail.com>
2022-10-22 11:45:46 +0100

    feat: configure admin

--- blogs/blog/admin.py
+++ blogs/blog/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Post
 
 # Register your models here.
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("title", "get_username", "body", "published_date", "status")
+    list_filter = ("status", "published_date")
+    search_fields = ("title", "body")
+    # TODO: 'prepopulated_fields' Do not work for updates
+    prepopulated_fields = {"slug": ("title",)}
+    date_hierarchy = "published_date"
+    ordering = ("status", "published_date")
+
+    @admin.display(description="Author")
+    def get_username(self, obj):
+        return obj.author.username
