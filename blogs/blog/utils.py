7687fb2e11f6315121b83046a1dce582e8696a41
Eyong Kevin Enowanyo <tonyparkerkenz@gmail.com>
2022-11-21 13:17:54 +0100

    refact: reduce text sizes

--- blogs/blog/utils.py
+++ blogs/blog/utils.py
@@ -13,7 +13,7 @@ def get_minimized_text(text, length=30):
     return text[:length] + "..."
 
 
-def pagination(request, object_list, page_limit=2):
+def pagination(request, object_list, page_limit=10):
     paginator = Paginator(object_list, page_limit)
     page = request.GET.get("page")
     try:
