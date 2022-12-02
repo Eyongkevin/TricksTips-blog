7226a71bc39c8f4c6221045a22809414e6a7d757
Eyong Kevin Enowanyo <tonyparkerkenz@gmail.com>
2022-12-02 21:26:26 +0100

    fix: mobile responsiveness

--- config/settings/base.py
+++ config/settings/base.py
@@ -118,13 +118,145 @@ customColorPalette = [
 
 # CKEDITOR_5_CUSTOM_CSS = 'path_to.css' # optional
 # CKEDITOR_5_FILE_STORAGE = "path_to_storage.CustomStorage" # optional
+# CKEDITOR_5_CONFIGS = {
+#     "default": {
+#         "skin": "moono",
+#         "toolbar": [
+#             "heading",
+#             "|",
+#             "bold",
+#             "italic",
+#             "link",
+#             "bulletedList",
+#             "numberedList",
+#             "blockQuote",
+#             "imageUpload",
+#         ],
+#     },
+#     "extends": {
+#         "blockToolbar": [
+#             "paragraph",
+#             "heading1",
+#             "heading2",
+#             "heading3",
+#             "|",
+#             "bulletedList",
+#             "numberedList",
+#             "|",
+#             "blockQuote",
+#         ],
+#         "toolbar": [
+#             "heading",
+#             "|",
+#             "outdent",
+#             "indent",
+#             "|",
+#             "bold",
+#             "italic",
+#             "link",
+#             "underline",
+#             "strikethrough",
+#             "code",
+#             "subscript",
+#             "superscript",
+#             "highlight",
+#             "|",
+#             "codeBlock",
+#             "sourceEditing",
+#             "insertImage",
+#             "bulletedList",
+#             "numberedList",
+#             "todoList",
+#             "|",
+#             "blockQuote",
+#             "imageUpload",
+#             "|",
+#             "fontSize",
+#             "fontFamily",
+#             "fontColor",
+#             "fontBackgroundColor",
+#             "mediaEmbed",
+#             "removeFormat",
+#             "insertTable",
+#         ],
+#         "image": {
+#             "toolbar": [
+#                 "imageTextAlternative",
+#                 "|",
+#                 "imageStyle:alignLeft",
+#                 "imageStyle:alignRight",
+#                 "imageStyle:alignCenter",
+#                 "imageStyle:side",
+#                 "|",
+#             ],
+#             "styles": [
+#                 "full",
+#                 "side",
+#                 "alignLeft",
+#                 "alignRight",
+#                 "alignCenter",
+#             ],
+#         },
+#         "table": {
+#             "contentToolbar": [
+#                 "tableColumn",
+#                 "tableRow",
+#                 "mergeTableCells",
+#                 "tableProperties",
+#                 "tableCellProperties",
+#             ],
+#             "tableProperties": {
+#                 "borderColors": customColorPalette,
+#                 "backgroundColors": customColorPalette,
+#             },
+#             "tableCellProperties": {
+#                 "borderColors": customColorPalette,
+#                 "backgroundColors": customColorPalette,
+#             },
+#         },
+#         "heading": {
+#             "options": [
+#                 {
+#                     "model": "paragraph",
+#                     "title": "Paragraph",
+#                     "class": "ck-heading_paragraph",
+#                 },
+#                 {
+#                     "model": "heading1",
+#                     "view": "h1",
+#                     "title": "Heading 1",
+#                     "class": "ck-heading_heading1",
+#                 },
+#                 {
+#                     "model": "heading2",
+#                     "view": "h2",
+#                     "title": "Heading 2",
+#                     "class": "ck-heading_heading2",
+#                 },
+#                 {
+#                     "model": "heading3",
+#                     "view": "h3",
+#                     "title": "Heading 3",
+#                     "class": "ck-heading_heading3",
+#                 },
+#             ]
+#         },
+#     },
+#     "list": {
+#         "properties": {
+#             "styles": "true",
+#             "startIndex": "true",
+#             "reversed": "true",
+#         }
+#     },
+# }
+
 CKEDITOR_5_CONFIGS = {
     "default": {
         "skin": "moono",
         "toolbar": [
             "heading",
             "|",
-            "bold",
             "italic",
             "link",
             "bulletedList",
@@ -151,6 +283,7 @@ CKEDITOR_5_CONFIGS = {
             "outdent",
             "indent",
             "|",
+            "linespace",
             "bold",
             "italic",
             "link",
@@ -242,11 +375,11 @@ CKEDITOR_5_CONFIGS = {
             ]
         },
     },
-    "list": {
-        "properties": {
-            "styles": "true",
-            "startIndex": "true",
-            "reversed": "true",
-        }
-    },
+    #     "list": {
+    #         "properties": {
+    #             "styles": "true",
+    #             "startIndex": "true",
+    #             "reversed": "true",
+    #         }
+    #     },
 }
