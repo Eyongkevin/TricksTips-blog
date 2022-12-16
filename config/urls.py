6c43a010e092e9a9a56b7fc9843af3922a968be0
Eyong Kevin Enowanyo <tonyparkerkenz@gmail.com>
2022-12-16 14:59:25 +0100

    refact: secure Django admin

--- config/urls.py
+++ config/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("mtt/admin/", admin.site.urls),
     path("", include("blogs.blog.urls", namespace="blogs")),
     path("unicorn/", include("django_unicorn.urls")),
     path("ckeditor5/", include("django_ckeditor_5.urls")),
